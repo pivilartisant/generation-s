5de5f6e7e72ff7b4a13ebd10c114dff383c9ceef
Pivi <pivi@pivis-air.home>
2023-05-10 00:24:28 +0200

    implement vinyl

--- client/src/Pages/Deck/Turnable/Turnable.jsx
+++ client/src/Pages/Deck/Turnable/Turnable.jsx
@@ -1,13 +1,21 @@
-// import { useEffect, useRef, useState } from 'react';
 import { Info } from "./Info/Info";
+import { Vinyl } from "./Vinyl/Vinyl";
 import { PlayButton } from "../Components/Buttons/PlayButton/PlayButton";
 import "./turnable.css";
 
 export function Turnable({...props}){
+
     return (
       <div className="turnable">
+        <div>
         <Info {...props}/>
+        </div>
+        <div className="middle-child">
+        <Vinyl size={380}/>
+        </div>
+        <div>
         <PlayButton {...props}/>
+        </div>
       </div>
     );
   };
---
+++ client/src/Pages/Deck/Turnable/Vinyl/Vinyl.jsx
@@ -0,0 +1,27 @@
+import img from "../../../../assets/booba.png"
+
+export function Vinyl ({...props}){
+    let { size } = props;   
+
+    const radius = 280; // radius of the circle
+    const diameter = radius * 2; // diameter of the circle
+    const imageSize = radius * 0.4; // size of the image (20% of the circumference)
+    const imageX = radius - (imageSize / 2); // X position of the image to center it
+    const imageY = radius - (imageSize / 2); // Y position of the image to center it
+
+    return (
+        <div className="vinyl"> 
+        <svg width={size} height={size} viewBox="0 0 570 570" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx={radius} cy={radius} r={radius} stroke="white" strokeWidth="2" fill="none" />
+         <image 
+            className="album-cover"
+            xlinkHref={img} 
+            x={imageX} 
+            y={imageY} 
+            width={imageSize} 
+            height={imageSize}
+            />
+        </svg>
+        </div>
+    )
+}
