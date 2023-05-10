f03d5c8a0904e293662c6a145422ff19b4cb2965
pivi <paul.lartisant@icloud.com>
2023-05-10 23:27:23 +0200

    change storejs value

--- client/src/store.js
+++ client/src/store.js
@@ -1,18 +1,15 @@
-import { create } from 'zustand'
-import { devtools, persist } from 'zustand/middleware'
+import { create } from "zustand";
+import { devtools, persist } from "zustand/middleware";
 
 const gameStore = (set) => ({
-  gameState: true,
-  
+  gameState: false,
+
   toggleGameState: () => {
-    console.log('Toggling game state');
-    set((state) => ({ gameState: !state.gameState}))}
-})
+    console.log("Toggling game state");
+    set((state) => ({ gameState: !state.gameState }));
+  },
+});
 
-const useGameStore = create(
-  devtools(
-    persist(
-      gameStore, { name: 'game' }
-  )));
+const useGameStore = create(devtools(persist(gameStore, { name: "game" })));
 
-export default useGameStore 
+export default useGameStore;
